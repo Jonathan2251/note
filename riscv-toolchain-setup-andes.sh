ef8dda8ddd43fbf72494f0930869429a82d8078c
ChungShu <gamma_chen@yahoo.com.tw>
2022-08-31 16:26:59 +0800

    riscv-toolchain-setup-andes.sh

---
+++ riscv-toolchain-setup-andes.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+# Verified on ubuntu 18.04
+# mkdir riscv/git, riscv/riscv_newlib, riscv_linux befor running this bash script
+export GNU_SRC_DIR=$HOME/Andes/riscv/git
+export LLVM_SRC_DIR=$HOME/Andes/riscv/llvm-package/source/llvm-project
+
+# More: install cmake from source since the version of cmake in Ubuntu 18.04 is 
+# too old.
+
+export GNU_NEWLIB_INSTALL_DIR=$HOME/Andes/riscv/riscv_newlib
+export LLVM_NEWLIB_BUILD_DIR=$LLVM_SRC_DIR/llvm-project/build_riscv_newlib
+
+export GNU_LINUX_INSTALL_DIR=$HOME/Andes/riscv/riscv_linux
+export LLVM_LINUX_BUILD_DIR=$LLVM_SRC_DIR/llvm-project/build_riscv_linux
+
+riscv_gnu_toolchain_prerequisites() {
+  sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev \
+  libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool \
+  patchutils bc zlib1g-dev libexpat-dev
+  if [ ! -f "/usr/bin/python" ]; then
+    sudo ln -s /usr/bin/python3 /usr/bin/python
+  fi
+}
+
+riscv_llvm_prerequisites() {
+  sudo apt-get install ninja-build
+}
+
+check() {
+  if [ ! -d "$GNU_SRC_DIR" ]; then
+    echo "GNU_SRC_DIR: $GNU_SRC_DIR not exist"
+    exit 1
+  fi
+  if [ -d "$GNU_NEWLIB_INSTALL_DIR" ]; then
+    echo "GNU_NEWLIB_INSTALL_DIR: $GNU_NEWLIB_INSTALL_DIR exist. Remove it before running."
+    exit 1
+  fi
+  if [ -d "$GNU_LINUX_INSTALL_DIR" ]; then
+    echo "GNU_LINUX_INSTALL_DIR: $GNU_LINUX_INSTALL_DIR exist. Remove it before running."
+    exit 1
+  fi
+}
+
+build_gnu_toolchain() {
+  pushd $GNU_SRC_DIR
+  git clone https://github.com/riscv/riscv-gnu-toolchain
+  cd riscv-gnu-toolchain
+#  Looks branch change from original/rvv-intrinsic to origin/__archive__
+#  git checkout -b rvv-intrinsic origin/rvv-intrinsic
+# commit 409b951ba6621f2f115aebddfb15ce2dd78ec24f of master branch is work for vadd.vv of vadd1.c
+  mkdir build_newlib
+  cd build_newlib
+  ../configure --prefix=$GNU_NEWLIB_INSTALL_DIR \
+  --with-multilib-generator="rv32i-ilp32--;rv32imafd-ilp32--;rv64ima-lp64--"
+  make
+
+  cd ..
+  mkdir build_linux
+  cd build_linux
+  ../configure --prefix=$GNU_LINUX_INSTALL_DIR
+  make linux
+  popd
+}
+
+# DEFAULT_SYSROOT: 
+#   https://stackoverflow.com/questions/66357013/compile-clang-with-alternative-sysroot
+# LLVM_DEFAULT_TARGET_TRIPLE:  
+#   https://clang.llvm.org/docs/CrossCompilation.html#general-cross-compilation-options-in-clang
+# Use "clang --sysroot" if did not "cmake -DDEFAULT_SYSROOT"
+# $GNU_NEWLIB_INSTALL_DIR/clang --gcc-toolchain=$GNU_NEWLIB_INSTALL_DIR \
+#   --sysroot=$GNU_NEWLIB_INSTALL_DIR/sysroot/ --static test.c
+build_llvm_toolchain() {
+  pushd $LLVM_SRC_DIR
+#  rm -rf build_newlib
+#  mkdir build_newlib
+#  cd build_newlib
+#  cmake -G "Ninja" ../llvm \
+#  -DCMAKE_INSTALL_PREFIX=$GNU_NEWLIB_INSTALL_DIR \
+#  -DLLVM_ENABLE_PROJECTS='clang' \
+#  -DCMAKE_BUILD_TYPE=Debug \
+#  -DLLVM_OPTIMIZED_TABLEGEN=On \
+#  -DLLVM_INSTALL_TOOLCHAIN_ONLY=Off \
+#  -DLLVM_TARGETS_TO_BUILD='RISCV' \
+#  -DLLVM_PARALLEL_COMPILE_JOBS=4 -DLLVM_PARALLEL_LINK_JOBS=1 \
+#  -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-elf
+#  ninja
+#  ninja install
+#  popd
+
+  mkdir build_riscv_newlib
+  mkdir build_riscv_linux
+  rm -rf build_riscv_newlib/* build_riscv_linux/*
+  pushd $LLVM_NEWLIB_BUILD_DIR
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld"  \
+  -DLLVM_OPTIMIZED_TABLEGEN=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=Off \
+  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include \
+  -DCMAKE_INSTALL_PREFIX=$GNU_NEWLIB_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-elf \
+  -DDEFAULT_SYSROOT=$GNU_NEWLIB_INSTALL_DIR/riscv64-unknown-elf \
+  -DLLVM_INSTALL_UTILS=ON ../llvm
+  ninja
+  ninja install
+  popd
+  pushd $LLVM_LINUX_BUILD_DIR
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD="RISCV" \
+  -DLLVM_ENABLE_PROJECTS="clang;lld"  \
+  -DLLVM_OPTIMIZED_TABLEGEN=On -DLLVM_INSTALL_TOOLCHAIN_ONLY=Off \
+  -DLLVM_BINUTILS_INCDIR=$GNU_SRC_DIR/riscv-gnu-toolchain/riscv-binutils/include \
+  -DCMAKE_INSTALL_PREFIX=$GNU_LINUX_INSTALL_DIR -DLLVM_PARALLEL_COMPILE_JOBS=4 \
+  -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-linux-gnu \
+  -DDEFAULT_SYSROOT=$GNU_LINUX_INSTALL_DIR/sysroot -DLLVM_INSTALL_UTILS=ON ../llvm
+  ninja
+  ninja install
+  popd
+}
+
+riscv_gnu_toolchain_prerequisites;
+riscv_llvm_prerequisites;
+#get_llvm;
+check;
+build_gnu_toolchain;
+build_llvm_toolchain;
