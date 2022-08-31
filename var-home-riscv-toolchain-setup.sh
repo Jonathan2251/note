c4e525e3f55927ba22fc808e2283baaa44b72d48
Jonathan Chen <gamma_chen@yahoo.com.tw>
2022-08-31 08:59:21 +0000

    move riscv-toolchain-setup to lbt/exlbt/riscv/riscv-toolchain-setup.sh

--- var-home-riscv-toolchain-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-GNU_SRC_DIR=$HOME/git/
-GNU_NEWLIB_INSTALL_DIR=$HOME/riscv_newlib
-LLVM_NEWLIB_BUILD_DIR=$HOME/llvm/llvm-project/build_riscv_newlib
-
-GNU_LINUX_INSTALL_DIR=$HOME/riscv_linux
-LLVM_LINUX_BUILD_DIR=$HOME/llvm/llvm-project/build_riscv_linux
