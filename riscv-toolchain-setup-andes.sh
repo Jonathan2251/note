951a4ed0fd03eb51a81de08681d45680f217b04d
Jonathan Chen <gamma_chen@yahoo.com.tw>
2022-09-19 09:04:31 +0000

    refine env var

--- riscv-toolchain-setup-andes.sh
+++ riscv-toolchain-setup-andes.sh
@@ -3,13 +3,13 @@
 # Verified on ubuntu 18.04
 # mkdir riscv/git, riscv/riscv_newlib, riscv_linux befor running this bash script
 export RISCV_DIR=$HOME/Andes/riscv
-export ANDES_LLVM_DIR=$HOME/Andes/riscv/llvm-package/source
-export LLVM_SRC_DIR=$HOME/Andes/riscv/llvm-project
+export ANDES_LLVM_DIR=$RISCV_DIR/llvm-package/source
+export LLVM_SRC_DIR=$RISCV_DIR/llvm-project
 
 # More: install cmake from source since the version of cmake in Ubuntu 18.04 is 
 # too old.
 
-export GNU_NEWLIB_INSTALL_DIR=$HOME/Andes/riscv/riscv_newlib
+export GNU_NEWLIB_INSTALL_DIR=$RISCV_DIR/nds64le-elf-newlib-v5d
 export LLVM_NEWLIB_BUILD_DIR=$LLVM_SRC_DIR/build_riscv_newlib
 
 riscv_gnu_toolchain_prerequisites() {
